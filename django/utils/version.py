bentan4853563/Django

commit 4a103086d5c67fa4fcc53c106c9fdf644c742dd8

Fixed #18269 -- Applied unicode_literals for Python 3 compatibility.
Thanks Vinay Sajip for the support of his django3 branch and
Jannis Leidel for the review.

django/utils/version.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import datetime
 import os
 import subprocess

commit 0e4fa8f2593019c3a35f24a4e90a7ffdd8ec3b1f

Ensured get_version returns a native string.
Returning unicode triggers a bug in Python 2.7:
http://bugs.python.org/issue11638

This problem was introduced in 4a103086 (unicode_literals).

django/utils/version.py
@@ -30,7 +30,7 @@ def get_version(version=None):
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
         sub = mapping[version[3]] + str(version[4])
 
-    return main + sub
+    return str(main + sub)
 
 def get_git_changeset():
     """Returns a numeric identifier of the latest git changeset.