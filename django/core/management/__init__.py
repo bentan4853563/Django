bentan4853563/Django

commit f011b903630b3167dc960829ec8c4cb5d46c2b94

Removed an old compatibility shim added in 1.0

django/core/management/__init__.py
@@ -13,9 +13,6 @@ from django.core.management.color import color_style
 from django.utils import lru_cache
 from django.utils import six
 
-# For backwards compatibility: get_version() used to be in this module.
-from django import get_version
-
 
 def find_commands(management_dir):
     """

commit 30eee3dbfca57c4453331e0e8c4870d05e3ecaca

Fix a regression introduced by f011b90363.
`django.get_version` was actually used by the module.

django/core/management/__init__.py
@@ -321,7 +321,7 @@ class ManagementUtility(object):
         # These options could affect the commands that are available, so they
         # must be processed early.
         parser = LaxOptionParser(usage="%prog subcommand [options] [args]",
-                                 version=get_version(),
+                                 version=django.get_version(),
                                  option_list=BaseCommand.option_list)
         try:
             options, args = parser.parse_args(self.argv)