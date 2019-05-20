bentan4853563/Django

commit 405c8363362063542e9e79beac53c8437d389520

Fixed #30385 -- Restored SearchVector(config) immutability.
Regression in 1a28dc3887e8d66d5e3ff08cf7fb0a6212b873e5.

The usage of CONCAT to allow SearchVector to deal with non-text fields
made the generated expression non-IMMUTABLE which prevents a functional
index to be created for it.

Using a combination of COALESCE and ::text makes sure the expression
preserves its immutability.

Refs #29582. Thanks Andrew Brown for the report, Nick Pope for the
review.

django/contrib/postgres/search.py
@@ -1,5 +1,6 @@
-from django.db.models import Field, FloatField
+from django.db.models import CharField, Field, FloatField, TextField
 from django.db.models.expressions import CombinedExpression, Func, Value
+from django.db.models.functions import Cast, Coalesce
 from django.db.models.lookups import Lookup
 
 
@@ -45,8 +46,7 @@ class SearchVectorCombinable:
 
 class SearchVector(SearchVectorCombinable, Func):
     function = 'to_tsvector'
-    arg_joiner = ", ' ',"
-    template = '%(function)s(concat(%(expressions)s))'
+    arg_joiner = " || ' ' || "
     output_field = SearchVectorField()
     config = None
 
@@ -60,6 +60,14 @@ class SearchVector(SearchVectorCombinable, Func):
 
     def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
         resolved = super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
+        resolved.set_source_expressions([
+            Coalesce(
+                expression
+                if isinstance(expression.output_field, (CharField, TextField))
+                else Cast(expression, TextField()),
+                Value('')
+            ) for expression in resolved.get_source_expressions()
+        ])
         if self.config:
             if not hasattr(self.config, 'resolve_expression'):
                 resolved.config = Value(self.config).resolve_expression(query, allow_joins, reuse, summarize, for_save)
@@ -72,7 +80,7 @@ class SearchVector(SearchVectorCombinable, Func):
         if template is None:
             if self.config:
                 config_sql, config_params = compiler.compile(self.config)
-                template = "%(function)s({}::regconfig, concat(%(expressions)s))".format(config_sql.replace('%', '%%'))
+                template = '%(function)s({}::regconfig, %(expressions)s)'.format(config_sql.replace('%', '%%'))
             else:
                 template = self.template
         sql, params = super().as_sql(compiler, connection, function=function, template=template)

tests/postgres_tests/test_search.py
@@ -8,6 +8,7 @@ transcript.
 from django.contrib.postgres.search import (
     SearchQuery, SearchRank, SearchVector,
 )
+from django.db import connection
 from django.db.models import F
 from django.test import SimpleTestCase, modify_settings, skipUnlessDBFeature
 
@@ -344,6 +345,23 @@ class TestRankingAndWeights(GrailTestData, PostgreSQLTestCase):
             rank=SearchRank(SearchVector('dialogue'), SearchQuery('brave sir robin')),
         ).filter(rank__gt=0.3)
         self.assertSequenceEqual(searched, [self.verse0])
+
+
+class SearchVectorIndexTests(PostgreSQLTestCase):
+    def test_search_vector_index(self):
+        """SearchVector generates IMMUTABLE SQL in order to be indexable."""
+        # This test should be moved to test_indexes and use a functional
+        # index instead once support lands (see #26167).
+        query = Line.objects.all().query
+        resolved = SearchVector('id', 'dialogue', config='english').resolve_expression(query)
+        compiler = query.get_compiler(connection.alias)
+        sql, params = resolved.as_sql(compiler, connection)
+        # Indexed function must be IMMUTABLE.
+        with connection.cursor() as cursor:
+            cursor.execute(
+                'CREATE INDEX search_vector_index ON %s USING GIN (%s)' % (Line._meta.db_table, sql),
+                params,
+            )
 
 
 class SearchQueryTests(SimpleTestCase):

commit 1d3d8f83e02ee94a636d65df815ec9a993cc895f

Fixed #30488 -- Removed redundant Coalesce call in SQL generated by SearchVector.
Regression in 405c8363362063542e9e79beac53c8437d389520.

django/contrib/postgres/search.py
@@ -60,14 +60,6 @@ class SearchVector(SearchVectorCombinable, Func):
 
     def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
         resolved = super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
-        resolved.set_source_expressions([
-            Coalesce(
-                expression
-                if isinstance(expression.output_field, (CharField, TextField))
-                else Cast(expression, TextField()),
-                Value('')
-            ) for expression in resolved.get_source_expressions()
-        ])
         if self.config:
             if not hasattr(self.config, 'resolve_expression'):
                 resolved.config = Value(self.config).resolve_expression(query, allow_joins, reuse, summarize, for_save)
@@ -76,17 +68,26 @@ class SearchVector(SearchVectorCombinable, Func):
         return resolved
 
     def as_sql(self, compiler, connection, function=None, template=None):
+        clone = self.copy()
+        clone.set_source_expressions([
+            Coalesce(
+                expression
+                if isinstance(expression.output_field, (CharField, TextField))
+                else Cast(expression, TextField()),
+                Value('')
+            ) for expression in clone.get_source_expressions()
+        ])
         config_params = []
         if template is None:
-            if self.config:
-                config_sql, config_params = compiler.compile(self.config)
+            if clone.config:
+                config_sql, config_params = compiler.compile(clone.config)
                 template = '%(function)s({}::regconfig, %(expressions)s)'.format(config_sql.replace('%', '%%'))
             else:
-                template = self.template
-        sql, params = super().as_sql(compiler, connection, function=function, template=template)
+                template = clone.template
+        sql, params = super(SearchVector, clone).as_sql(compiler, connection, function=function, template=template)
         extra_params = []
-        if self.weight:
-            weight_sql, extra_params = compiler.compile(self.weight)
+        if clone.weight:
+            weight_sql, extra_params = compiler.compile(clone.weight)
             sql = 'setweight({}, {})'.format(sql, weight_sql)
         return sql, config_params + params + extra_params
 

tests/postgres_tests/test_search.py
@@ -113,6 +113,10 @@ class SearchVectorFieldTest(GrailTestData, PostgreSQLTestCase):
         searched = Line.objects.filter(dialogue_search_vector=SearchQuery('cadeaux', config='french'))
         self.assertSequenceEqual(searched, [self.french])
 
+    def test_single_coalesce_expression(self):
+        searched = Line.objects.annotate(search=SearchVector('dialogue')).filter(search='cadeaux')
+        self.assertNotIn('COALESCE(COALESCE', str(searched.query))
+
 
 class MultipleFieldsTest(GrailTestData, PostgreSQLTestCase):
 