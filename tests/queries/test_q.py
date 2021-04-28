bentan4853563/Django

commit bbf141bcdc31f1324048af9233583a523ac54c94

Refs #27149 -- Fixed sql.Query identity.
By making Query subclass BaseExpression in
35431298226165986ad07e91f9d3aca721ff38ec the former defined it's
identity based off _construct_args which is not appropriate.

django/db/models/sql/query.py
@@ -39,6 +39,7 @@ from django.db.models.sql.where import (
 )
 from django.utils.deprecation import RemovedInDjango40Warning
 from django.utils.functional import cached_property
+from django.utils.hashable import make_hashable
 from django.utils.tree import Node
 
 __all__ = ['Query', 'RawQuery']
@@ -245,6 +246,14 @@ class Query(BaseExpression):
     def base_table(self):
         for alias in self.alias_map:
             return alias
+
+    @property
+    def identity(self):
+        identity = (
+            (arg, make_hashable(value))
+            for arg, value in self.__dict__.items()
+        )
+        return (self.__class__, *identity)
 
     def __str__(self):
         """

tests/queries/test_query.py
@@ -150,3 +150,31 @@ class TestQuery(SimpleTestCase):
         msg = 'Cannot filter against a non-conditional expression.'
         with self.assertRaisesMessage(TypeError, msg):
             query.build_where(Func(output_field=CharField()))
+
+    def test_equality(self):
+        self.assertNotEqual(
+            Author.objects.all().query,
+            Author.objects.filter(item__name='foo').query,
+        )
+        self.assertEqual(
+            Author.objects.filter(item__name='foo').query,
+            Author.objects.filter(item__name='foo').query,
+        )
+        self.assertEqual(
+            Author.objects.filter(item__name='foo').query,
+            Author.objects.filter(Q(item__name='foo')).query,
+        )
+
+    def test_hash(self):
+        self.assertNotEqual(
+            hash(Author.objects.all().query),
+            hash(Author.objects.filter(item__name='foo').query)
+        )
+        self.assertEqual(
+            hash(Author.objects.filter(item__name='foo').query),
+            hash(Author.objects.filter(item__name='foo').query),
+        )
+        self.assertEqual(
+            hash(Author.objects.filter(item__name='foo').query),
+            hash(Author.objects.filter(Q(item__name='foo')).query),
+        )

django/db/models/lookups.py
@@ -12,6 +12,7 @@ from django.db.models.query_utils import RegisterLookupMixin
 from django.utils.datastructures import OrderedSet
 from django.utils.deprecation import RemovedInDjango40Warning
 from django.utils.functional import cached_property
+from django.utils.hashable import make_hashable
 
 
 class Lookup:
@@ -142,6 +143,18 @@ class Lookup:
     @property
     def is_summary(self):
         return self.lhs.is_summary or getattr(self.rhs, 'is_summary', False)
+
+    @property
+    def identity(self):
+        return self.__class__, self.lhs, self.rhs
+
+    def __eq__(self, other):
+        if not isinstance(other, Lookup):
+            return NotImplemented
+        return self.identity == other.identity
+
+    def __hash__(self):
+        return hash(make_hashable(self.identity))
 
 
 class Transform(RegisterLookupMixin, Func):

django/db/models/sql/datastructures.py
@@ -114,17 +114,28 @@ class Join:
             self.join_field, self.nullable, filtered_relation=filtered_relation,
         )
 
-    def equals(self, other, with_filtered_relation):
+    @property
+    def identity(self):
         return (
-            isinstance(other, self.__class__) and
-            self.table_name == other.table_name and
-            self.parent_alias == other.parent_alias and
-            self.join_field == other.join_field and
-            (not with_filtered_relation or self.filtered_relation == other.filtered_relation)
+            self.__class__,
+            self.table_name,
+            self.parent_alias,
+            self.join_field,
+            self.filtered_relation,
         )
 
     def __eq__(self, other):
-        return self.equals(other, with_filtered_relation=True)
+        if not isinstance(other, Join):
+            return NotImplemented
+        return self.identity == other.identity
+
+    def __hash__(self):
+        return hash(self.identity)
+
+    def equals(self, other, with_filtered_relation):
+        if with_filtered_relation:
+            return self == other
+        return self.identity[:-1] == other.identity[:-1]
 
     def demote(self):
         new = self.relabeled_clone({})
@@ -160,9 +171,17 @@ class BaseTable:
     def relabeled_clone(self, change_map):
         return self.__class__(self.table_name, change_map.get(self.table_alias, self.table_alias))
 
+    @property
+    def identity(self):
+        return self.__class__, self.table_name, self.table_alias
+
+    def __eq__(self, other):
+        if not isinstance(other, BaseTable):
+            return NotImplemented
+        return self.identity == other.identity
+
+    def __hash__(self):
+        return hash(self.identity)
+
     def equals(self, other, with_filtered_relation):
-        return (
-            isinstance(self, other.__class__) and
-            self.table_name == other.table_name and
-            self.table_alias == other.table_alias
-        )
+        return self.identity == other.identity

tests/lookup/test_lookups.py
@@ -1,8 +1,32 @@
 from datetime import datetime
+from unittest import mock
 
 from django.db.models import DateTimeField, Value
-from django.db.models.lookups import YearLookup
+from django.db.models.lookups import Lookup, YearLookup
 from django.test import SimpleTestCase
+
+
+class CustomLookup(Lookup):
+    pass
+
+
+class LookupTests(SimpleTestCase):
+    def test_equality(self):
+        lookup = Lookup(Value(1), Value(2))
+        self.assertEqual(lookup, lookup)
+        self.assertEqual(lookup, Lookup(lookup.lhs, lookup.rhs))
+        self.assertEqual(lookup, mock.ANY)
+        self.assertNotEqual(lookup, Lookup(lookup.lhs, Value(3)))
+        self.assertNotEqual(lookup, Lookup(Value(3), lookup.rhs))
+        self.assertNotEqual(lookup, CustomLookup(lookup.lhs, lookup.rhs))
+
+    def test_hash(self):
+        lookup = Lookup(Value(1), Value(2))
+        self.assertEqual(hash(lookup), hash(lookup))
+        self.assertEqual(hash(lookup), hash(Lookup(lookup.lhs, lookup.rhs)))
+        self.assertNotEqual(hash(lookup), hash(Lookup(lookup.lhs, Value(3))))
+        self.assertNotEqual(hash(lookup), hash(Lookup(Value(3), lookup.rhs)))
+        self.assertNotEqual(hash(lookup), hash(CustomLookup(lookup.lhs, lookup.rhs)))
 
 
 class YearLookupTests(SimpleTestCase):

commit 78f6031ed3651b731f3708d3709d70588196ce98

Fixed #32632, Fixed #32657 -- Removed flawed support for Subquery deconstruction.
Subquery deconstruction support required implementing complex and
expensive equality rules for sql.Query objects for little benefit as
the latter cannot themselves be made deconstructible to their reference
to model classes.

Making Expression @deconstructible and not BaseExpression allows
interested parties to conform to the "expression" API even if they are
not deconstructible as it's only a requirement for expressions allowed
in Model fields and meta options (e.g. constraints, indexes).

Thanks Phillip Cutter for the report.

This also fixes a performance regression in bbf141bcdc31f1324048af9233583a523ac54c94.

django/db/models/expressions.py
@@ -147,7 +147,6 @@ class Combinable:
         )
 
 
-@deconstructible
 class BaseExpression:
     """Base class for all query expressions."""
 
@@ -389,6 +388,11 @@ class BaseExpression:
             return self.output_field.select_format(compiler, sql, params)
         return sql, params
 
+
+@deconstructible
+class Expression(BaseExpression, Combinable):
+    """An expression that can be combined with other expressions."""
+
     @cached_property
     def identity(self):
         constructor_signature = inspect.signature(self.__init__)
@@ -409,17 +413,12 @@ class BaseExpression:
         return tuple(identity)
 
     def __eq__(self, other):
-        if not isinstance(other, BaseExpression):
+        if not isinstance(other, Expression):
             return NotImplemented
         return other.identity == self.identity
 
     def __hash__(self):
         return hash(self.identity)
-
-
-class Expression(BaseExpression, Combinable):
-    """An expression that can be combined with other expressions."""
-    pass
 
 
 _connector_combinators = {
@@ -1103,7 +1102,7 @@ class Case(Expression):
         return super().get_group_by_cols(alias)
 
 
-class Subquery(Expression):
+class Subquery(BaseExpression, Combinable):
     """
     An explicit subquery. It may contain OuterRef() references to the outer
     query which will be resolved when it is applied to that query.
@@ -1116,16 +1115,6 @@ class Subquery(Expression):
         self.query = getattr(queryset, 'query', queryset)
         self.extra = extra
         super().__init__(output_field)
-
-    def __getstate__(self):
-        state = super().__getstate__()
-        args, kwargs = state['_constructor_args']
-        if args:
-            args = (self.query, *args[1:])
-        else:
-            kwargs['queryset'] = self.query
-        state['_constructor_args'] = args, kwargs
-        return state
 
     def get_source_expressions(self):
         return [self.query]
@@ -1203,6 +1192,7 @@ class Exists(Subquery):
         return sql, params
 
 
+@deconstructible
 class OrderBy(BaseExpression):
     template = '%(expression)s %(ordering)s'
     conditional = False

django/db/models/query_utils.py
@@ -5,6 +5,7 @@ Factored out from django.db.models.query to avoid making the main module very
 large and/or so that they can be used by other modules without getting into
 circular import difficulties.
 """
+import copy
 import functools
 import inspect
 from collections import namedtuple
@@ -43,14 +44,11 @@ class Q(tree.Node):
         if not(isinstance(other, Q) or getattr(other, 'conditional', False) is True):
             raise TypeError(other)
 
-        # If the other Q() is empty, ignore it and just use `self`.
-        if not other:
+        if not self:
+            return other.copy() if hasattr(other, 'copy') else copy.copy(other)
+        elif isinstance(other, Q) and not other:
             _, args, kwargs = self.deconstruct()
             return type(self)(*args, **kwargs)
-        # Or if this Q is empty, ignore it and just use `other`.
-        elif not self:
-            _, args, kwargs = other.deconstruct()
-            return type(other)(*args, **kwargs)
 
         obj = type(self)()
         obj.connector = conn

django/db/models/sql/query.py
@@ -36,7 +36,6 @@ from django.db.models.sql.where import (
     AND, OR, ExtraWhere, NothingNode, WhereNode,
 )
 from django.utils.functional import cached_property
-from django.utils.hashable import make_hashable
 from django.utils.tree import Node
 
 __all__ = ['Query', 'RawQuery']
@@ -249,14 +248,6 @@ class Query(BaseExpression):
     def base_table(self):
         for alias in self.alias_map:
             return alias
-
-    @property
-    def identity(self):
-        identity = (
-            (arg, make_hashable(value))
-            for arg, value in self.__dict__.items()
-        )
-        return (self.__class__, *identity)
 
     def __str__(self):
         """

tests/queries/test_q.py
@@ -1,4 +1,5 @@
-from django.db.models import Exists, F, OuterRef, Q
+from django.db.models import BooleanField, Exists, F, OuterRef, Q
+from django.db.models.expressions import RawSQL
 from django.test import SimpleTestCase
 
 from .models import Tag
@@ -49,6 +50,16 @@ class QTests(SimpleTestCase):
             q | obj
         with self.assertRaisesMessage(TypeError, str(obj)):
             q & obj
+
+    def test_combine_negated_boolean_expression(self):
+        tagged = Tag.objects.filter(category=OuterRef('pk'))
+        tests = [
+            Q() & ~Exists(tagged),
+            Q() | ~Exists(tagged),
+        ]
+        for q in tests:
+            with self.subTest(q=q):
+                self.assertIs(q.negated, True)
 
     def test_deconstruct(self):
         q = Q(price__gt=F('discounted_price'))
@@ -101,10 +112,10 @@ class QTests(SimpleTestCase):
         self.assertEqual(kwargs, {})
 
     def test_deconstruct_boolean_expression(self):
-        tagged = Tag.objects.filter(category=OuterRef('pk'))
-        q = Q(Exists(tagged))
+        expr = RawSQL('1 = 1', BooleanField())
+        q = Q(expr)
         _, args, kwargs = q.deconstruct()
-        self.assertEqual(args, (Exists(tagged),))
+        self.assertEqual(args, (expr,))
         self.assertEqual(kwargs, {})
 
     def test_reconstruct(self):

tests/queries/test_query.py
@@ -150,31 +150,3 @@ class TestQuery(SimpleTestCase):
         msg = 'Cannot filter against a non-conditional expression.'
         with self.assertRaisesMessage(TypeError, msg):
             query.build_where(Func(output_field=CharField()))
-
-    def test_equality(self):
-        self.assertNotEqual(
-            Author.objects.all().query,
-            Author.objects.filter(item__name='foo').query,
-        )
-        self.assertEqual(
-            Author.objects.filter(item__name='foo').query,
-            Author.objects.filter(item__name='foo').query,
-        )
-        self.assertEqual(
-            Author.objects.filter(item__name='foo').query,
-            Author.objects.filter(Q(item__name='foo')).query,
-        )
-
-    def test_hash(self):
-        self.assertNotEqual(
-            hash(Author.objects.all().query),
-            hash(Author.objects.filter(item__name='foo').query)
-        )
-        self.assertEqual(
-            hash(Author.objects.filter(item__name='foo').query),
-            hash(Author.objects.filter(item__name='foo').query),
-        )
-        self.assertEqual(
-            hash(Author.objects.filter(item__name='foo').query),
-            hash(Author.objects.filter(Q(item__name='foo')).query),
-        )