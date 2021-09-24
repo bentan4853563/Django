bentan4853563/Django

commit 456466d932830b096d39806e291fe23ec5ed38d5

Fixed #31026 -- Switched form rendering to template engine.
Thanks Carlton Gibson, Keryn Knight, Mariusz Felisiak, and Nick Pope
for reviews.

Co-authored-by: Johannes Hoppe <info@johanneshoppe.com>

django/forms/utils.py
@@ -1,10 +1,12 @@
 import json
-from collections import UserList
+from collections import UserDict, UserList
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
+from django.forms.renderers import get_default_renderer
 from django.utils import timezone
-from django.utils.html import escape, format_html, format_html_join, html_safe
+from django.utils.html import escape, format_html_join
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
 
@@ -41,53 +43,90 @@ def flatatt(attrs):
     )
 
 
-@html_safe
-class ErrorDict(dict):
+class RenderableMixin:
+    def get_context(self):
+        raise NotImplementedError(
+            'Subclasses of RenderableMixin must provide a get_context() method.'
+        )
+
+    def render(self, template_name=None, context=None, renderer=None):
+        return mark_safe((renderer or self.renderer).render(
+            template_name or self.template_name,
+            context or self.get_context(),
+        ))
+
+    __str__ = render
+    __html__ = render
+
+
+class RenderableFormMixin(RenderableMixin):
+    def as_p(self):
+        """Render as <p> elements."""
+        return self.render(self.template_name_p)
+
+    def as_table(self):
+        """Render as <tr> elements excluding the surrounding <table> tag."""
+        return self.render(self.template_name_table)
+
+    def as_ul(self):
+        """Render as <li> elements excluding the surrounding <ul> tag."""
+        return self.render(self.template_name_ul)
+
+
+class RenderableErrorMixin(RenderableMixin):
+    def as_json(self, escape_html=False):
+        return json.dumps(self.get_json_data(escape_html))
+
+    def as_text(self):
+        return self.render(self.template_name_text)
+
+    def as_ul(self):
+        return self.render(self.template_name_ul)
+
+
+class ErrorDict(UserDict, RenderableErrorMixin):
     """
     A collection of errors that knows how to display itself in various formats.
 
     The dictionary keys are the field names, and the values are the errors.
     """
+    template_name = 'django/forms/errors/dict/default.html'
+    template_name_text = 'django/forms/errors/dict/text.txt'
+    template_name_ul = 'django/forms/errors/dict/ul.html'
+
+    def __init__(self, data=None, renderer=None):
+        super().__init__(data)
+        self.renderer = renderer or get_default_renderer()
+
     def as_data(self):
         return {f: e.as_data() for f, e in self.items()}
 
     def get_json_data(self, escape_html=False):
         return {f: e.get_json_data(escape_html) for f, e in self.items()}
 
-    def as_json(self, escape_html=False):
-        return json.dumps(self.get_json_data(escape_html))
-
-    def as_ul(self):
-        if not self:
-            return ''
-        return format_html(
-            '<ul class="errorlist">{}</ul>',
-            format_html_join('', '<li>{}{}</li>', self.items())
-        )
-
-    def as_text(self):
-        output = []
-        for field, errors in self.items():
-            output.append('* %s' % field)
-            output.append('\n'.join('  * %s' % e for e in errors))
-        return '\n'.join(output)
-
-    def __str__(self):
-        return self.as_ul()
+    def get_context(self):
+        return {
+            'errors': self.items(),
+            'error_class': 'errorlist',
+        }
 
 
-@html_safe
-class ErrorList(UserList, list):
+class ErrorList(UserList, list, RenderableErrorMixin):
     """
     A collection of errors that knows how to display itself in various formats.
     """
-    def __init__(self, initlist=None, error_class=None):
+    template_name = 'django/forms/errors/list/default.html'
+    template_name_text = 'django/forms/errors/list/text.txt'
+    template_name_ul = 'django/forms/errors/list/ul.html'
+
+    def __init__(self, initlist=None, error_class=None, renderer=None):
         super().__init__(initlist)
 
         if error_class is None:
             self.error_class = 'errorlist'
         else:
             self.error_class = 'errorlist {}'.format(error_class)
+        self.renderer = renderer or get_default_renderer()
 
     def as_data(self):
         return ValidationError(self.data).error_list
@@ -107,24 +146,11 @@ class ErrorList(UserList, list):
             })
         return errors
 
-    def as_json(self, escape_html=False):
-        return json.dumps(self.get_json_data(escape_html))
-
-    def as_ul(self):
-        if not self.data:
-            return ''
-
-        return format_html(
-            '<ul class="{}">{}</ul>',
-            self.error_class,
-            format_html_join('', '<li>{}</li>', ((e,) for e in self))
-        )
-
-    def as_text(self):
-        return '\n'.join('* %s' % e for e in self)
-
-    def __str__(self):
-        return self.as_ul()
+    def get_context(self):
+        return {
+            'errors': self,
+            'error_class': self.error_class,
+        }
 
     def __repr__(self):
         return repr(list(self))

tests/forms_tests/tests/test_forms.py
@@ -23,6 +23,7 @@ from django.test import SimpleTestCase
 from django.test.utils import override_settings
 from django.utils.datastructures import MultiValueDict
 from django.utils.safestring import mark_safe
+from tests.forms_tests.tests import test_all_form_renderers
 
 
 class FrameworkForm(Form):
@@ -55,6 +56,7 @@ class MultiValueDictLike(dict):
         return [self[key]]
 
 
+@test_all_form_renderers()
 class FormsTestCase(SimpleTestCase):
     # A Form is a collection of Fields. It knows how to validate a set of data and it
     # knows how to render itself in a couple of default ways (e.g., an HTML table).
@@ -3077,117 +3079,6 @@ Password: <input type="password" name="password" required>
 
         self.assertHTMLEqual(boundfield.label_tag(label_suffix='$'), '<label for="id_field">Field$</label>')
 
-    def test_field_name(self):
-        """#5749 - `field_name` may be used as a key in _html_output()."""
-        class SomeForm(Form):
-            some_field = CharField()
-
-            def as_p(self):
-                return self._html_output(
-                    normal_row='<p id="p_%(field_name)s"></p>',
-                    error_row='%s',
-                    row_ender='</p>',
-                    help_text_html=' %s',
-                    errors_on_separate_row=True,
-                )
-
-        form = SomeForm()
-        self.assertHTMLEqual(form.as_p(), '<p id="p_some_field"></p>')
-
-    def test_field_without_css_classes(self):
-        """
-        `css_classes` may be used as a key in _html_output() (empty classes).
-        """
-        class SomeForm(Form):
-            some_field = CharField()
-
-            def as_p(self):
-                return self._html_output(
-                    normal_row='<p class="%(css_classes)s"></p>',
-                    error_row='%s',
-                    row_ender='</p>',
-                    help_text_html=' %s',
-                    errors_on_separate_row=True,
-                )
-
-        form = SomeForm()
-        self.assertHTMLEqual(form.as_p(), '<p class=""></p>')
-
-    def test_field_with_css_class(self):
-        """
-        `css_classes` may be used as a key in _html_output() (class comes
-        from required_css_class in this case).
-        """
-        class SomeForm(Form):
-            some_field = CharField()
-            required_css_class = 'foo'
-
-            def as_p(self):
-                return self._html_output(
-                    normal_row='<p class="%(css_classes)s"></p>',
-                    error_row='%s',
-                    row_ender='</p>',
-                    help_text_html=' %s',
-                    errors_on_separate_row=True,
-                )
-
-        form = SomeForm()
-        self.assertHTMLEqual(form.as_p(), '<p class="foo"></p>')
-
-    def test_field_name_with_hidden_input(self):
-        """
-        BaseForm._html_output() should merge all the hidden input fields and
-        put them in the last row.
-        """
-        class SomeForm(Form):
-            hidden1 = CharField(widget=HiddenInput)
-            custom = CharField()
-            hidden2 = CharField(widget=HiddenInput)
-
-            def as_p(self):
-                return self._html_output(
-                    normal_row='<p%(html_class_attr)s>%(field)s %(field_name)s</p>',
-                    error_row='%s',
-                    row_ender='</p>',
-                    help_text_html=' %s',
-                    errors_on_separate_row=True,
-                )
-
-        form = SomeForm()
-        self.assertHTMLEqual(
-            form.as_p(),
-            '<p><input id="id_custom" name="custom" type="text" required> custom'
-            '<input id="id_hidden1" name="hidden1" type="hidden">'
-            '<input id="id_hidden2" name="hidden2" type="hidden"></p>'
-        )
-
-    def test_field_name_with_hidden_input_and_non_matching_row_ender(self):
-        """
-        BaseForm._html_output() should merge all the hidden input fields and
-        put them in the last row ended with the specific row ender.
-        """
-        class SomeForm(Form):
-            hidden1 = CharField(widget=HiddenInput)
-            custom = CharField()
-            hidden2 = CharField(widget=HiddenInput)
-
-            def as_p(self):
-                return self._html_output(
-                    normal_row='<p%(html_class_attr)s>%(field)s %(field_name)s</p>',
-                    error_row='%s',
-                    row_ender='<hr><hr>',
-                    help_text_html=' %s',
-                    errors_on_separate_row=True
-                )
-
-        form = SomeForm()
-        self.assertHTMLEqual(
-            form.as_p(),
-            '<p><input id="id_custom" name="custom" type="text" required> custom</p>\n'
-            '<input id="id_hidden1" name="hidden1" type="hidden">'
-            '<input id="id_hidden2" name="hidden2" type="hidden"><hr><hr>'
-        )
-
     def test_error_dict(self):
         class MyForm(Form):
             foo = CharField()
@@ -3376,30 +3267,6 @@ Password: <input type="password" name="password" required>
 <tr><th><label for="id_last_name">Last name:</label></th><td>
 <input id="id_last_name" name="last_name" type="text" value="Lennon" required></td></tr>"""
         )
-
-    def test_errorlist_override(self):
-        class DivErrorList(ErrorList):
-            def __str__(self):
-                return self.as_divs()
-
-            def as_divs(self):
-                if not self:
-                    return ''
-                return '<div class="errorlist">%s</div>' % ''.join(
-                    '<div class="error">%s</div>' % e for e in self)
-
-        class CommentForm(Form):
-            name = CharField(max_length=50, required=False)
-            email = EmailField()
-            comment = CharField()
-
-        data = {'email': 'invalid'}
-        f = CommentForm(data, auto_id=False, error_class=DivErrorList)
-        self.assertHTMLEqual(f.as_p(), """<p>Name: <input type="text" name="name" maxlength="50"></p>
-<div class="errorlist"><div class="error">Enter a valid email address.</div></div>
-<p>Email: <input type="email" name="email" value="invalid" required></p>
-<div class="errorlist"><div class="error">This field is required.</div></div>
-<p>Comment: <input type="text" name="comment" required></p>""")
 
     def test_error_escaping(self):
         class TestForm(Form):
@@ -4044,4 +3911,41 @@ class TemplateTests(SimpleTestCase):
             }),
             "VALID: [('password1', 'secret'), ('password2', 'secret'), "
             "('username', 'adrian')]",
+        )
+
+
+class OverrideTests(SimpleTestCase):
+    def test_use_custom_template(self):
+        class Person(Form):
+            first_name = CharField()
+            template_name = 'forms_tests/form_snippet.html'
+
+        t = Template('{{ form }}')
+        html = t.render(Context({'form': Person()}))
+        expected = """
+        <div class="fieldWrapper"><label for="id_first_name">First name:</label>
+        <input type="text" name="first_name" required id="id_first_name"></div>
+        """
+        self.assertHTMLEqual(html, expected)
+
+    def test_errorlist_override(self):
+        class CustomErrorList(ErrorList):
+            template_name = 'forms_tests/error.html'
+
+        class CommentForm(Form):
+            name = CharField(max_length=50, required=False)
+            email = EmailField()
+            comment = CharField()
+
+        data = {'email': 'invalid'}
+        f = CommentForm(data, auto_id=False, error_class=CustomErrorList)
+        self.assertHTMLEqual(
+            f.as_p(),
+            '<p>Name: <input type="text" name="name" maxlength="50"></p>'
+            '<div class="errorlist">'
+            '<div class="error">Enter a valid email address.</div></div>'
+            '<p>Email: <input type="email" name="email" value="invalid" required></p>'
+            '<div class="errorlist">'
+            '<div class="error">This field is required.</div></div>'
+            '<p>Comment: <input type="text" name="comment" required></p>',
         )

django/forms/boundfield.py
@@ -1,11 +1,10 @@
 import re
 
 from django.core.exceptions import ValidationError
-from django.forms.utils import flatatt, pretty_name
+from django.forms.utils import pretty_name
 from django.forms.widgets import MultiWidget, Textarea, TextInput
 from django.utils.functional import cached_property
-from django.utils.html import conditional_escape, format_html, html_safe
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html, html_safe
 from django.utils.translation import gettext_lazy as _
 
 __all__ = ('BoundField',)
@@ -75,7 +74,7 @@ class BoundField:
         """
         Return an ErrorList (empty if there are no errors) for this field.
         """
-        return self.form.errors.get(self.name, self.form.error_class())
+        return self.form.errors.get(self.name, self.form.error_class(renderer=self.form.renderer))
 
     def as_widget(self, widget=None, attrs=None, only_initial=False):
         """
@@ -177,11 +176,14 @@ class BoundField:
                     attrs['class'] += ' ' + self.form.required_css_class
                 else:
                     attrs['class'] = self.form.required_css_class
-            attrs = flatatt(attrs) if attrs else ''
-            contents = format_html('<label{}>{}</label>', attrs, contents)
-        else:
-            contents = conditional_escape(contents)
-        return mark_safe(contents)
+        context = {
+            'form': self.form,
+            'field': self,
+            'label': contents,
+            'attrs': attrs,
+            'use_tag': bool(id_),
+        }
+        return self.form.render(self.form.template_name_label, context)
 
     def css_classes(self, extra_classes=None):
         """

django/forms/forms.py
@@ -4,15 +4,17 @@ Form classes
 
 import copy
 import datetime
+import warnings
 
 from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
 from django.forms.fields import Field, FileField
-from django.forms.utils import ErrorDict, ErrorList
+from django.forms.utils import ErrorDict, ErrorList, RenderableFormMixin
 from django.forms.widgets import Media, MediaDefiningClass
 from django.utils.datastructures import MultiValueDict
+from django.utils.deprecation import RemovedInDjango50Warning
 from django.utils.functional import cached_property
-from django.utils.html import conditional_escape, html_safe
-from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+from django.utils.safestring import SafeString, mark_safe
 from django.utils.translation import gettext as _
 
 from .renderers import get_default_renderer
@@ -49,8 +51,7 @@ class DeclarativeFieldsMetaclass(MediaDefiningClass):
         return new_class
 
 
-@html_safe
-class BaseForm:
+class BaseForm(RenderableFormMixin):
     """
     The main implementation of all the Form logic. Note that this class is
     different than Form. See the comments by the Form class for more info. Any
@@ -61,6 +62,12 @@ class BaseForm:
     field_order = None
     prefix = None
     use_required_attribute = True
+
+    template_name = 'django/forms/default.html'
+    template_name_p = 'django/forms/p.html'
+    template_name_table = 'django/forms/table.html'
+    template_name_ul = 'django/forms/ul.html'
+    template_name_label = 'django/forms/label.html'
 
     def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None,
                  initial=None, error_class=ErrorList, label_suffix=None,
@@ -128,9 +135,6 @@ class BaseForm:
                 pass
         fields.update(self.fields)  # add remaining fields in original order
         self.fields = fields
-
-    def __str__(self):
-        return self.as_table()
 
     def __repr__(self):
         if self._errors is None:
@@ -206,6 +210,12 @@ class BaseForm:
 
     def _html_output(self, normal_row, error_row, row_ender, help_text_html, errors_on_separate_row):
         "Output HTML. Used by as_table(), as_ul(), as_p()."
+        warnings.warn(
+            'django.forms.BaseForm._html_output() is deprecated. '
+            'Please use .render() and .get_context() instead.',
+            RemovedInDjango50Warning,
+            stacklevel=2,
+        )
         # Errors that should be displayed above all fields.
         top_errors = self.non_field_errors().copy()
         output, hidden_fields = [], []
@@ -282,35 +292,37 @@ class BaseForm:
                 output.append(str_hidden)
         return mark_safe('\n'.join(output))
 
-    def as_table(self):
-        "Return this form rendered as HTML <tr>s -- excluding the <table></table>."
-        return self._html_output(
-            normal_row='<tr%(html_class_attr)s><th>%(label)s</th><td>%(errors)s%(field)s%(help_text)s</td></tr>',
-            error_row='<tr><td colspan="2">%s</td></tr>',
-            row_ender='</td></tr>',
-            help_text_html='<br><span class="helptext">%s</span>',
-            errors_on_separate_row=False,
-        )
-
-    def as_ul(self):
-        "Return this form rendered as HTML <li>s -- excluding the <ul></ul>."
-        return self._html_output(
-            normal_row='<li%(html_class_attr)s>%(errors)s%(label)s %(field)s%(help_text)s</li>',
-            error_row='<li>%s</li>',
-            row_ender='</li>',
-            help_text_html=' <span class="helptext">%s</span>',
-            errors_on_separate_row=False,
-        )
-
-    def as_p(self):
-        "Return this form rendered as HTML <p>s."
-        return self._html_output(
-            normal_row='<p%(html_class_attr)s>%(label)s %(field)s%(help_text)s</p>',
-            error_row='%s',
-            row_ender='</p>',
-            help_text_html=' <span class="helptext">%s</span>',
-            errors_on_separate_row=True,
-        )
+    def get_context(self):
+        fields = []
+        hidden_fields = []
+        top_errors = self.non_field_errors().copy()
+        for name, bf in self._bound_items():
+            bf_errors = self.error_class(bf.errors, renderer=self.renderer)
+            if bf.is_hidden:
+                if bf_errors:
+                    top_errors += [
+                        _('(Hidden field %(name)s) %(error)s') % {'name': name, 'error': str(e)}
+                        for e in bf_errors
+                    ]
+                hidden_fields.append(bf)
+            else:
+                errors_str = str(bf_errors)
+                # RemovedInDjango50Warning.
+                if not isinstance(errors_str, SafeString):
+                    warnings.warn(
+                        f'Returning a plain string from '
+                        f'{self.error_class.__name__} is deprecated. Please '
+                        f'customize via the template system instead.',
+                        RemovedInDjango50Warning,
+                    )
+                    errors_str = mark_safe(errors_str)
+                fields.append((bf, errors_str))
+        return {
+            'form': self,
+            'fields': fields,
+            'hidden_fields': hidden_fields,
+            'errors': top_errors,
+        }
 
     def non_field_errors(self):
         """
@@ -318,7 +330,10 @@ class BaseForm:
         field -- i.e., from Form.clean(). Return an empty ErrorList if there
         are none.
         """
-        return self.errors.get(NON_FIELD_ERRORS, self.error_class(error_class='nonfield'))
+        return self.errors.get(
+            NON_FIELD_ERRORS,
+            self.error_class(error_class='nonfield', renderer=self.renderer),
+        )
 
     def add_error(self, field, error):
         """
@@ -360,9 +375,9 @@ class BaseForm:
                     raise ValueError(
                         "'%s' has no field named '%s'." % (self.__class__.__name__, field))
                 if field == NON_FIELD_ERRORS:
-                    self._errors[field] = self.error_class(error_class='nonfield')
+                    self._errors[field] = self.error_class(error_class='nonfield', renderer=self.renderer)
                 else:
-                    self._errors[field] = self.error_class()
+                    self._errors[field] = self.error_class(renderer=self.renderer)
             self._errors[field].extend(error_list)
             if field in self.cleaned_data:
                 del self.cleaned_data[field]

commit 4623e30640585c7ec778bbe9aa87afdb5cfe3dfa

Fixed #33130 -- Restored form errors to be a dict.
Regression in 456466d932830b096d39806e291fe23ec5ed38d5.

django/forms/utils.py
@@ -1,5 +1,5 @@
 import json
-from collections import UserDict, UserList
+from collections import UserList
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -84,7 +84,7 @@ class RenderableErrorMixin(RenderableMixin):
         return self.render(self.template_name_ul)
 
 
-class ErrorDict(UserDict, RenderableErrorMixin):
+class ErrorDict(dict, RenderableErrorMixin):
     """
     A collection of errors that knows how to display itself in various formats.
 
@@ -94,8 +94,8 @@ class ErrorDict(UserDict, RenderableErrorMixin):
     template_name_text = 'django/forms/errors/dict/text.txt'
     template_name_ul = 'django/forms/errors/dict/ul.html'
 
-    def __init__(self, data=None, renderer=None):
-        super().__init__(data)
+    def __init__(self, *args, renderer=None, **kwargs):
+        super().__init__(*args, **kwargs)
         self.renderer = renderer or get_default_renderer()
 
     def as_data(self):

tests/forms_tests/tests/test_forms.py
@@ -67,6 +67,7 @@ class FormsTestCase(SimpleTestCase):
 
         self.assertTrue(p.is_bound)
         self.assertEqual(p.errors, {})
+        self.assertIsInstance(p.errors, dict)
         self.assertTrue(p.is_valid())
         self.assertHTMLEqual(p.errors.as_ul(), '')
         self.assertEqual(p.errors.as_text(), '')

tests/forms_tests/tests/test_utils.py
@@ -1,4 +1,5 @@
 import copy
+import json
 
 from django.core.exceptions import ValidationError
 from django.forms.utils import ErrorDict, ErrorList, flatatt
@@ -162,3 +163,48 @@ class FormsUtilsTestCase(SimpleTestCase):
         e = ErrorList(['Invalid username.'])
         self.assertTrue(hasattr(ErrorList, '__html__'))
         self.assertEqual(str(e), e.__html__())
+
+    def test_error_dict_is_dict(self):
+        self.assertIsInstance(ErrorDict(), dict)
+
+    def test_error_dict_is_json_serializable(self):
+        init_errors = ErrorDict([
+            ('__all__', ErrorList([
+                ValidationError('Sorry this form only works on leap days.')
+            ])),
+            ('name', ErrorList([ValidationError('This field is required.')])),
+        ])
+        min_value_error_list = ErrorList([
+            ValidationError('Ensure this value is greater than or equal to 0.')
+        ])
+        e = ErrorDict(
+            init_errors,
+            date=ErrorList([
+                ErrorDict({
+                    'day': min_value_error_list,
+                    'month': min_value_error_list,
+                    'year': min_value_error_list,
+                }),
+            ]),
+        )
+        e['renderer'] = ErrorList([
+            ValidationError(
+                'Select a valid choice. That choice is not one of the '
+                'available choices.'
+            ),
+        ])
+        self.assertJSONEqual(json.dumps(e), {
+            '__all__': ['Sorry this form only works on leap days.'],
+            'name': ['This field is required.'],
+            'date': [
+                {
+                    'day': ['Ensure this value is greater than or equal to 0.'],
+                    'month': ['Ensure this value is greater than or equal to 0.'],
+                    'year': ['Ensure this value is greater than or equal to 0.'],
+                },
+            ],
+            'renderer': [
+                'Select a valid choice. That choice is not one of the '
+                'available choices.'
+            ],
+        })