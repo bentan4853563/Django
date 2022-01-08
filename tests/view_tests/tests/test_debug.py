bentan4853563/Django

commit 0c0b87725bbcffca3bc3a7a2c649995695a5ae3b

Refs #32260 -- Made admindocs and technical 404 debug page use view_func.view_class.
Internals of admindocs and technical 404 debug page should use the
view_class attribute and do not rely on __name__.

django/views/debug.py
@@ -517,7 +517,9 @@ def technical_404_response(request, exception):
     else:
         obj = resolver_match.func
 
-        if hasattr(obj, '__name__'):
+        if hasattr(obj, 'view_class'):
+            caller = obj.view_class
+        elif hasattr(obj, '__name__'):
             caller = obj.__name__
         elif hasattr(obj, '__class__') and hasattr(obj.__class__, '__name__'):
             caller = obj.__class__.__name__

django/contrib/admindocs/utils.py
@@ -20,6 +20,9 @@ else:
 
 
 def get_view_name(view_func):
+    if hasattr(view_func, 'view_class'):
+        klass = view_func.view_class
+        return f'{klass.__module__}.{klass.__qualname__}'
     mod_name = view_func.__module__
     view_name = getattr(view_func, '__qualname__', view_func.__class__.__name__)
     return mod_name + '.' + view_name

django/urls/resolvers.py
@@ -378,7 +378,9 @@ class URLPattern:
         callback = self.callback
         if isinstance(callback, functools.partial):
             callback = callback.func
-        if not hasattr(callback, '__name__'):
+        if hasattr(callback, 'view_class'):
+            callback = callback.view_class
+        elif not hasattr(callback, '__name__'):
             return callback.__module__ + "." + callback.__class__.__name__
         return callback.__module__ + "." + callback.__qualname__
 

commit 0bb2df6586e7d205238b44cf0555b53ba5c884ac

Fixed #33425 -- Fixed view name for CBVs on technical 404 debug page.
Regression in 0c0b87725bbcffca3bc3a7a2c649995695a5ae3b.

django/views/debug.py
@@ -545,8 +545,9 @@ def technical_404_response(request, exception):
         obj = resolver_match.func
 
         if hasattr(obj, 'view_class'):
-            caller = obj.view_class
-        elif hasattr(obj, '__name__'):
+            obj = obj.view_class
+
+        if hasattr(obj, '__name__'):
             caller = obj.__name__
         elif hasattr(obj, '__class__') and hasattr(obj.__class__, '__name__'):
             caller = obj.__class__.__name__

tests/view_tests/tests/test_debug.py
@@ -177,7 +177,11 @@ class DebugViewTests(SimpleTestCase):
             html=True,
         )
         self.assertContains(response, "Raised by:", status_code=404)
-        self.assertContains(response, "view_tests.views.technical404", status_code=404)
+        self.assertContains(
+            response,
+            '<td>view_tests.views.technical404</td>',
+            status_code=404,
+        )
         self.assertContains(
             response,
             '<p>The current path, <code>technical404/</code>, matched the '
@@ -188,8 +192,12 @@ class DebugViewTests(SimpleTestCase):
 
     def test_classbased_technical_404(self):
         response = self.client.get('/classbased404/')
-        self.assertContains(response, "Raised by:", status_code=404)
-        self.assertContains(response, "view_tests.views.Http404View", status_code=404)
+        self.assertContains(
+            response,
+            '<th>Raised by:</th><td>view_tests.views.Http404View</td>',
+            status_code=404,
+            html=True,
+        )
 
     def test_non_l10ned_numeric_ids(self):
         """