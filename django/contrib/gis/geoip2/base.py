bentan4853563/Django

commit 31bef51d8ea5e3bf650de1495ae1f0566670b8f3

Moved unnecessary inner import in GeoIP2.geos().

django/contrib/gis/geoip2/base.py
@@ -3,6 +3,7 @@ import socket
 import geoip2.database
 
 from django.conf import settings
+from django.contrib.gis.geos import Point
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_ipv46_address
 from django.utils._os import to_path
@@ -210,7 +211,6 @@ class GeoIP2:
         "Return a GEOS Point object for the given query."
         ll = self.lon_lat(query)
         if ll:
-            from django.contrib.gis.geos import Point
             return Point(ll, srid=4326)
         else:
             return None

commit 31641a9edd87e43a6fbedd0cbea2bd55686e9f45

Fixed #33886 -- Reallowed using GeoIP2() when GEOS is not installed.
Regression in 31bef51d8ea5e3bf650de1495ae1f0566670b8f3.

django/contrib/gis/geoip2/base.py
@@ -3,7 +3,6 @@ import socket
 import geoip2.database
 
 from django.conf import settings
-from django.contrib.gis.geos import Point
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_ipv46_address
 from django.utils._os import to_path
@@ -219,6 +218,9 @@ class GeoIP2:
         "Return a GEOS Point object for the given query."
         ll = self.lon_lat(query)
         if ll:
+            # Allows importing and using GeoIP2() when GEOS is not installed.
+            from django.contrib.gis.geos import Point
+
             return Point(ll, srid=4326)
         else:
             return None